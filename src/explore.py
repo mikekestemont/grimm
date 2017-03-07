f2f8773a208a2f9a46ab9779acfb0938d18184a1
Mike Kestemont <mike.kestemont@gmail.com>
2017-03-07 09:55:01 +0100

    parse script

--- src/explore.py
+++ src/explore.py
@@ -1,10 +1,15 @@
 import os
 from glob import glob
-from collections import namedtuple
+from collections import namedtuple, Counter
 
-def load_letters(bpath='../brothers-grimm-data/'):
+from vectorization import Vectorizer
+from sklearn.decomposition import PCA
+
+
+def load_letters(bpath='../brothers-grimm-data/', start_from_line=3):
     Letter = namedtuple('letter', ['author', 'addressee',
-                                    'day', 'month', 'year', 'words'])
+                                    'day', 'month', 'year',
+                                    'words'])
 
     letters = []
 
@@ -19,7 +24,7 @@ def load_letters(bpath='../brothers-grimm-data/'):
             #y = int(''.join([c for c in y if c.isdigit()]))
 
             with open(fp) as f:
-                lines = [l.strip() for l in f.readlines()][1:]
+                lines = [l.strip() for l in f.readlines()][start_from_line:]
                 words = ' '.join(lines).split()
 
             letters.append(Letter(send, addr, d, m, y, words))
@@ -29,7 +34,7 @@ def load_letters(bpath='../brothers-grimm-data/'):
 
     return letters
 
-def filter_letters(letters, min_len=50,
+def filter_letters(letters, min_len=500,
                     target_authors={'Jacob-Grimm', 'Wilhelm-Grimm'}):
     
     letters = [l for l in letters if len(l.words) >= min_len]
@@ -37,12 +42,38 @@ def filter_letters(letters, min_len=50,
 
     return letters
 
+def pca(X, labels):
+    prin_comp = PCA(n_components=2)
+    pca_matrix = prin_comp.fit_transform(X.toarray()) # unsparsify
+    pca_loadings = prin_comp.components_.transpose()
+
+    
 
 
 def main():
+    # load
     letters = load_letters()
-    letters = filter_letters(letters)
-    print(len(letters))
+    letters = filter_letters(letters, min_len=500)
+    print('Loaded:')
+    print(Counter([l.author for l in letters]))
+
+    for l in letters:
+        fn = l.author + '_' + '-'.join([l.day, l.month, l.year]) + '.txt'
+        text = ' '.join(l.words)
+        with open('clean/'+fn, 'w') as f:
+            f.write(text)
+
+    # vectorize
+    vectorizer = Vectorizer(mfi=150, ngram_type='word',
+                 ngram_size=1, vocabulary=None,
+                 vector_space='tf_std', lowercase=True,
+                 min_df=0.0, max_df=1.0, ignore=[])
+
+    X = vectorizer.fit_transform([l.words for l in letters])
+    print(vectorizer.feature_names)
+
+    pca(X, [l.author for l in letters])
+    
 
 if __name__ == '__main__':
     main()
