a7ff68ac786f52521a714d0e66cc4caffae4f028
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2017-03-15 18:13:41 +0100

    wip on pretraining multihead lm

--- src/lm/pretrain.py
+++ src/lm/pretrain.py
@@ -17,41 +17,17 @@ torch.manual_seed(seed)
 
 import torch.nn as nn
 
-from modules import ForkableLM, MultiheadLM, LMContainer
+from modules import LM, LMContainer
 from optimizer import Optimizer
-from dataset import Dict, CyclicBlockDataset
+from dataset import Dict, BlockDataset
 from early_stopping import EarlyStoppingException
 import utils as u
-
-
-def train_model_fork(
-        model, train, valid, test, optim, epochs, criterion, d, **kwargs):
-    print("Training main")
-    test_ppl = u.train_model(
-        model, train, valid, test, optim, epochs, criterion, d, **kwargs)
-    model_J, model_W = model.fork_model(), model.fork_model()
-    for label, model in [('J', model_J), ('W', model_W)]:
-        print("Training brother [%s]" % label)
-        optim.set_params(model.parameters())
-        u.train_model(
-            model, train, valid, test, optim, epochs, criterion, d, **kwargs)
-    return {'J': model_J, 'W': model_W}, test_ppl
-
-
-def train_model_multihead(
-        model, train, valid, test, optim, epochs, criterion, d, **kwargs):
-    print("Training main")
-    test_ppl = u.train_model(
-        model, train, valid, test, optim, epochs, criterion, d, **kwargs)
-    return model, test_ppl
 
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--path', required=True)
-    parser.add_argument('--already_split', action='store_true')
-    parser.add_argument('--training_mode', required=True)
     parser.add_argument('--max_size', default=1000000, type=int)
     parser.add_argument('--min_freq', default=1, type=int)
     parser.add_argument('--layers', default=2, type=int)
@@ -59,8 +35,6 @@ if __name__ == '__main__':
     parser.add_argument('--emb_dim', default=16, type=int)
     parser.add_argument('--hid_dim', default=248, type=int)
     parser.add_argument('--dropout', default=0.3, type=float)
-    parser.add_argument('--tie_weights', action='store_true')
-    parser.add_argument('--project_on_tied_weights', action='store_true')
     parser.add_argument('--batch_size', default=200, type=int)
     parser.add_argument('--bptt', default=10, type=int)
     parser.add_argument('--reset_hidden', action='store_true')
@@ -79,38 +53,19 @@ if __name__ == '__main__':
     parser.add_argument('--prefix', default='model', type=str)
     args = parser.parse_args()
 
-    print('Loading data...')
-
     sys.path.append('../')
-    from src.utils import filter_letters, load_letters, split, letters2lines
+    from src.utils import load_letters, letters2lines
 
-    def load_files(**kwargs):
-        bpath = os.path.expanduser(args.path)
-        letters = load_letters(bpath=bpath, **kwargs)
-        return split(filter_letters(letters, min_len=0))
-
+    print("Loading data...")
+    letters = load_letters(
+        bpath=os.path.expanduser(args.path), subset='', start_from_line=0)
+    lines = letters2lines(letters)
     d = Dict(max_size=args.max_size, min_freq=args.min_freq,
              bos_token=u.BOS, eos_token=u.EOS)
-
-    if args.already_split:      # fetch already splitted datasets
-        train_J, train_W = load_files(subset='train/', start_from_line=0)
-        train_J, train_W = letters2lines(train_J), letters2lines(train_W)
-        test_J, test_W = load_files(subset='test/', start_from_line=0)
-        test_J, test_W = letters2lines(test_J), letters2lines(test_W)
-        d.fit(train_J, train_W)
-        test = CyclicBlockDataset(
-            {'J': test_J, 'W': test_W}, d, args.batch_size, args.bptt,
-            gpu=args.gpu, evaluation=True)
-        train, valid = CyclicBlockDataset(
-            {'J': train_J, 'W': train_W}, d, args.batch_size, args.bptt,
-            gpu=args.gpu).splits(test=0.1, dev=None)
-    else:                       # fetch raw datasets computing splits
-        J, W = load_files()
-        J, W = letters2lines(J), letters2lines(W)
-        d.fit(J, W)
-        train, test, valid = CyclicBlockDataset(
-            {'J': J, 'W': W}, d, args.batch_size, args.bptt,
-            gpu=args.gpu).splits(dev=0.1)
+    d.fit(lines)
+    train, test, valid = BlockDataset(
+        lines, d, args.batch_size, args.bptt, gpu=args.gpu
+    ).splits(test=0.1, dev=0.1)
 
     print(' * vocabulary size. %d' % len(d))
 
@@ -118,22 +73,9 @@ if __name__ == '__main__':
     print(" * number of test batches. %d" % (len(test) * args.bptt))
     print(" * number of valid batches. %d" % (len(valid) * args.bptt))
 
-    print('Building model...')
-
-    opts = {"num_layers": args.layers, "dropout": args.dropout,
-            "tie_weights": args.tie_weights,
-            "project_on_tied_weights": args.project_on_tied_weights}
-
-    if args.training_mode.startswith('fork'):
-        model_type, train_fn = ForkableLM, train_model_fork
-    elif args.training_mode.startswith('multi'):
-        model_type, train_fn = MultiheadLM, train_model_multihead
-        opts.update({'heads': ('W', 'J')})
-    else:
-        raise ValueError("Unknown training mode [%s]" % args.training_mode)
-
-    model = model_type(
-        len(d), args.emb_dim, args.hid_dim, cell=args.cell, **opts)
+    print("Building model...")
+    model = LM(len(d), args.emb_dim, args.hid_dim, cell=args.cell,
+               num_layers=args.layers, dropout=args.dropout)
 
     model.apply(u.make_initializer())
 
@@ -148,8 +90,8 @@ if __name__ == '__main__':
 
     try:
         start = time.time()
-        trained_models, test_ppl = train_fn(
-            model, train, valid, test, optim, args.epochs, criterion, d,
+        trained_models, test_ppl = u.train_model(
+            model, train, valid, test, optim, args.epochs, criterion,
             gpu=args.gpu, early_stop=args.early_stop, hook=args.hook,
             checkpoint=args.checkpoint, reset_hidden=args.reset_hidden)
         if args.save:
